2423c9ea24635b136769b236f4eb5e354ae8ac03
Connor Ferris <ferr0373@gmail.com>
2023-01-20 20:56:25 -0600

    addEmployee form functional

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ $(document).ready(onReady);
 
 //set handlers
 function onReady() {
+console.log('initializing handlers...');
 
 //form submit handler
 $('#add-employee-form').on('submit', addEmployee);
@@ -17,34 +18,37 @@ render();
 
 //initialize global variables
 let employees = [ 
-    { firstName : 'Samuel', lastName : 'Malone' , id : 1, title : 'Owner/Bartender', annualSalary : '$90,000'},
-    { firstName : 'Diane', lastName : 'Chambers', id : 124, title : 'Server', annualSalary : '$44,600' },
-    { firstName : 'Ernie', lastName : 'Pantusso', id : 13, title : 'Assistant bartender', annualSalary : '$51,000' },
-    { firstName : 'Carla', lastName : 'Tortelli', id : 18, title : 'Server', annualSalary : '$46,800' }
+    { firstName : 'Samuel', lastName : 'Malone' , id : 1, title : 'Owner/Bartender', annualSalary : 90000},
+    { firstName : 'Diane', lastName : 'Chambers', id : 124, title : 'Server', annualSalary : 44600 },
+    { firstName : 'Ernie', lastName : 'Pantusso', id : 13, title : 'Assistant bartender', annualSalary : 51000 },
+    { firstName : 'Carla', lastName : 'Tortelli', id : 18, title : 'Server', annualSalary : 46800 }
 ];
+console.log('initialized Employees', employees);
 
 
-//creates new employee object and adds it to the end of the array
-function addEmployee() {
+//creates new employee object, adds it to the end of the array, then calls render
+function addEmployee(evt) {
+
+    evt.preventDefault();
     let employee = {
         firstName : $('#firstNameInput').val(),
         lastName :  $('#lastNameInput').val(),
         id : $('#idInput').val(),
-        title : $('salaryInput').val()
+        title : $('#titleInput').val(),
+        annualSalary : $('#salaryInput').val()
     };
     employees.push(employee);
-
-    render();
     console.log('employee added', employees);
+    render();
 }
 
 
-/*function deleteEmployee() {
+function deleteEmployee() {
 
-} */
+}
 
+//render function updates dynamic web page elements
 function render() {
-
 
     $('#employee-table').empty();
 
@@ -55,8 +59,15 @@ function render() {
                 <td>${employee.lastName}</td>
                 <td>${employee.id}</td>
                 <td>${employee.title}</td>
-                <td>${employee.annualSalary}</td>
+                <td>$${employee.annualSalary}</td>
             </tr>`
-        )
+        );
     }
+
+    //empty field inputs
+    $('#firstNameInput').val(''),
+    $('#lastNameInput').val(''),
+    $('#idInput').val(''),
+    $('#titleInput').val(''),
+    $('#salaryInput').val('')
 }
